1b9151c58b8759e57051c6b0b0484c3c326828c1
Marco Rossini <marco.rossini.1994@gmail.com>
2018-07-10 18:17:02 +0200

    - Added condition while calculating robot moves time to avoid bad values

--- it.unibo.finaltask/src/it/unibo/cleaner/cleanerTime.java
+++ it.unibo.finaltask/src/it/unibo/cleaner/cleanerTime.java
@@ -13,7 +13,10 @@ public class cleanerTime {
 	public static void stopTime(QActor qa) {
 		long stopTime = System.nanoTime();
 		long timeMoved = (stopTime - startTime) / 1000000; // conversion from nano to millis for readability reasons
-		qa.addRule("timeMoved(" + Long.toString(timeMoved) + ")");
+		
+		if (timeMoved > 0)
+			qa.addRule("timeMoved(" + Long.toString(timeMoved) + ")");
+		
 		startTime = -1; // resetting this value to negative to help debugging
 	}
 }
